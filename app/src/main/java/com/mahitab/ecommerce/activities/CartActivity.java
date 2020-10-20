5e9837f1d90009e5c4c361c032366199ab721115
Ahmed Abdelfattah <ahmed.a.elfattah.ahmed@gmail.com>
2020-10-20 17:59:01 +0200

    implement CartFragment not finished and CartActivity

---
+++ app/src/main/java/com/mahitab/ecommerce/activities/CartActivity.java
@@ -0,0 +1,41 @@
+package com.mahitab.ecommerce.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.mahitab.ecommerce.R;
+
+import static com.mahitab.ecommerce.utils.CommonUtils.setArDefaultLocale;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setArDefaultLocale(this);
+        setContentView(R.layout.activity_cart);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getResources().getString(R.string.cart));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        overridePendingTransition(0, 0); // remove activity default transition
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+}
