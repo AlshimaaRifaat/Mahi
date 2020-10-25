a8f6c4c2c0ba30db52cb26852feaca76bc23dd57
AlshimaaRifaat <alshimaa.rifaat71@gmail.com>
2020-10-25 14:55:02 +0200

    handle logout and Account data

--- app/src/main/java/com/mahitab/ecommerce/fragments/AccountFragment.java
+++ app/src/main/java/com/mahitab/ecommerce/fragments/AccountFragment.java
@@ -11,17 +11,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.mahitab.ecommerce.R;
 import com.mahitab.ecommerce.activities.HomeActivity;
 import com.mahitab.ecommerce.activities.LoginActivity;
 import com.mahitab.ecommerce.activities.RegisterActivity;
 import com.mahitab.ecommerce.managers.GraphClientManager;
+import com.mahitab.ecommerce.models.CurrentUser;
 import com.shopify.buy3.GraphCall;
 import com.shopify.buy3.GraphError;
 import com.shopify.buy3.GraphResponse;
@@ -35,7 +38,7 @@ public class AccountFragment extends Fragment {
     private Button btnLogin;
     private Button btnRegister;
     private TextView tvCustomerName;
-    private LinearLayout llLoginRegister;
+    private LinearLayout llLoginRegister,ll_customer_account_data;
     private Button btnSignOut;
 
     private String email;
@@ -43,7 +46,7 @@ public class AccountFragment extends Fragment {
     private String accessToken;
 
     private SharedPreferences defaultPreferences;
-
+    SharedPreferences sharedPreferences;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -59,15 +62,18 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view= inflater.inflate(R.layout.fragment_account, container, false);
+
+        return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         initView(view);
-
+        getSavedAccessToken();
         if (getActivity() != null && getActivity() instanceof HomeActivity) {
             ((HomeActivity) getActivity()).setSupportActionBar(toolbar);
             setHasOptionsMenu(true);
@@ -84,12 +90,37 @@ public class AccountFragment extends Fragment {
             toolbar.setTitle(getResources().getString(R.string.account));
         }
         email = defaultPreferences.getString("email", null);
-        password = defaultPreferences.getString("email", null);
+        password = defaultPreferences.getString("password", null);
 
-        if (email != null && password != null) {
-            createAccessToken();
+
+    }
+    private void getSavedAccessToken() {
+
+             sharedPreferences = getActivity().getSharedPreferences(getActivity().getPackageName(), Context.MODE_PRIVATE);
+             accessToken = sharedPreferences.getString("token", null);
+             Log.d(TAG, "getSavedAccessToken: " + accessToken);
+        if(accessToken!=null) {
+             fetchCustomerQuery(accessToken);
+
+            llLoginRegister.setVisibility(View.GONE);
+            btnSignOut.setVisibility(View.VISIBLE);
+            ll_customer_account_data.setVisibility(View.VISIBLE);
+            btnSignOut.setOnClickListener(onClickListener -> {
+                defaultPreferences.edit().putString("token", null).apply();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                getActivity().finish();
+                getActivity().runOnUiThread(() -> {
+                    Toast.makeText(getContext(), "You are Logged Out Successfuly!", Toast.LENGTH_SHORT).show();
+                });
+            });
+
+        }else{
+            llLoginRegister.setVisibility(View.VISIBLE);
+            btnSignOut.setVisibility(View.GONE);
+            ll_customer_account_data.setVisibility(View.GONE);
         }
     }
+
 
     private void initView(View view) {
         toolbar = view.findViewById(R.id.toolbar);
@@ -98,53 +129,12 @@ public class AccountFragment extends Fragment {
         tvCustomerName = view.findViewById(R.id.tvCustomerName_AccountFragment);
         llLoginRegister = view.findViewById(R.id.llLoginRegister_AccountFragment);
         btnSignOut = view.findViewById(R.id.btnSignOut_AccountFragment);
+        ll_customer_account_data= view.findViewById(R.id.ll_customer_account_data);
+
     }
 
-    private void createAccessToken() {
-        Log.d(TAG, "createAccessToken :e " + email);
-        Log.d(TAG, "createAccessToken:p " + password);
-        Storefront.CustomerAccessTokenCreateInput tokenCreateInput = new Storefront.CustomerAccessTokenCreateInput(email, password);
 
-        Storefront.MutationQuery mutationQuery = Storefront.mutation(mutation -> mutation
-                .customerAccessTokenCreate(tokenCreateInput, query -> query
-                        .customerAccessToken(customerAccessToken -> customerAccessToken
-                                .accessToken()
-                                .expiresAt()
-
-                        )
-                        .userErrors(userError -> userError
-                                .field()
-                                .message()
-                        )
-                )
-        );
-        getAccessTokenFromAPI(mutationQuery);
-    }
-
-    private void getAccessTokenFromAPI(Storefront.MutationQuery mutationQuery) {
-        GraphClientManager.mClient.mutateGraph(mutationQuery).enqueue(new GraphCall.Callback<Storefront.Mutation>() {
-            @Override
-            public void onResponse(@NonNull GraphResponse<Storefront.Mutation> response) {
-                if (!response.data().getCustomerAccessTokenCreate().getUserErrors().isEmpty()) {
-                    for (Storefront.UserError error : response.data().getCustomerAccessTokenCreate().getUserErrors()) {
-                        Log.e(TAG, "error is " + error.getMessage());
-                    }
-                } else {
-                    accessToken = response.data().getCustomerAccessTokenCreate().getCustomerAccessToken().getAccessToken();
-                    Log.e(TAG, "login" + response.data().getCustomerAccessTokenCreate().getCustomerAccessToken().getAccessToken());
-                    // queryUserDetails(accessToken);
-                    fetchCustomerQuery(accessToken);
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull GraphError error) {
-                Log.d(TAG, "Create customer Account API FAIL:" + error.getMessage());
-            }
-        });
-    }
-
-    private void fetchCustomerQuery(String accessToken) {
+   private void fetchCustomerQuery(String accessToken) {
         Storefront.QueryRootQuery queryRootQuery = Storefront.query(rootQuery -> rootQuery
                 .customer(
                         accessToken,
@@ -202,7 +192,14 @@ public class AccountFragment extends Fragment {
             public void onResponse(@NonNull GraphResponse<Storefront.QueryRoot> response) {
                 if (response.data() != null) {
                     Storefront.Customer customer = response.data().getCustomer();
-                    tvCustomerName.setText(customer.getFirstName());
+
+                    if(response.data().getCustomer().getFirstName()!=null)
+                    {
+                        tvCustomerName.setText(customer.getFirstName());
+                        Log.e("data", "user..." + response.data().getCustomer().getFirstName().toString());
+                    }
+
+
                     Log.e(TAG, "onResponse: getDisplayName" + customer.getDisplayName() + " getFirstName " + customer.getFirstName() + " getLastName" + customer.getLastName());
                 }else Log.e(TAG, "onResponse: here" );
             }
